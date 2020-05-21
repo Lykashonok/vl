1d2d1bd51d7eb041e8d96bdb0b686fb979327fd4
Lykashonok <dembyvlad@gmail.com>
2020-05-21 13:57:29 +0300

    wrong group deleting

--- vl/apps/lab_queue/admin.py
+++ vl/apps/lab_queue/admin.py
@@ -2,8 +2,36 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Queue, Profile, UserInQueue
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import Queue, Profile, UserInQueue, Message, Chat
+
+class MessageInline(admin.TabularInline):
+    model = Message
+
+class MessageAdmin(admin.ModelAdmin):
+    inlines = [
+        MessageInline
+    ]
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Queue)
-admin.site.register(Profile)
 admin.site.register(UserInQueue)
+admin.site.register(Message)
+admin.site.register(Chat, MessageAdmin)
